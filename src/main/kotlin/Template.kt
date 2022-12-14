59ae1f6ae21b7db085b7bc90d9604c85b0b818a2
Mark Kotyk <mkotyk@gmail.com>
2022-12-14 08:15:40 -0500

    Day 13

---
+++ src/main/kotlin/Template.kt
@@ -0,0 +1,31 @@
+fun main() {
+    val sampleInput =
+        """
+        """.trimIndent()
+
+    val puzzleInput = "dayXX/input.txt".loadResource()
+
+    fun String.parseData(): List<String> = this.split('\n')
+
+    // Part 1
+    fun List<String>.part1(): Int =0
+
+    with(sampleInput.parseData()) {
+        Assert.equals(part1(), 0)
+    }
+
+    with(puzzleInput.parseData()) {
+        println("Part 1 result: ${part1()}")
+    }
+
+    // Part 2
+    fun List<String>.part2(): Int  = 0
+
+    with(sampleInput.parseData()) {
+        Assert.equals(part2(), 0)
+    }
+
+    with(puzzleInput.parseData()) {
+        println("Part 2 result: ${part2()}")
+    }
+}

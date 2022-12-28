abf4be4f5564b6f71b993576400046bea0874f7e
Mark Kotyk <mkotyk@gmail.com>
2022-12-28 17:03:05 -0500

    Day 22

--- src/main/kotlin/Day22.kt
+++ src/main/kotlin/Day22.kt
@@ -1,9 +1,22 @@
 import java.util.*
 
 sealed class Instruction {
-    data class Move(val amount: Int) : Instruction()
-    data class Turn(val direction: Direction) : Instruction()
+    data class Forward(val count: Int) : Instruction()
+    object Left : Instruction()
+    object Right : Instruction()
 }
+
+data class Board(
+    val map: Bitmap,
+    val initialPosition: Point,
+    val perimeter: List<Location>,
+    val walls: Set<Point>,
+    val adjacent: Map<Location, Location> = emptyMap()
+)
+
+data class Location(val position: Point, val direction: Direction = Direction.None)
+
+infix operator fun Point.rangeTo(end: Point) = Line(this, end)
 
 operator fun Direction.plus(other: Direction): Direction {
     return when (other) {
@@ -12,6 +25,7 @@ operator fun Direction.plus(other: Direction): Direction {
             Direction.Right -> Direction.Top
             Direction.Top -> Direction.Left
             Direction.Bottom -> Direction.Right
+            Direction.None -> this
             else -> error("Unhandled directional change")
         }
 
@@ -20,19 +34,12 @@ operator fun Direction.plus(other: Direction): Direction {
             Direction.Right -> Direction.Bottom
             Direction.Top -> Direction.Right
             Direction.Bottom -> Direction.Left
+            Direction.None -> this
             else -> error("Unhandled directional change")
         }
 
-        else -> error("Unhandled directional change")
-    }
-}
+        Direction.None -> this
 
-fun Direction.opposite(): Direction {
-    return when (this) {
-        Direction.Left -> Direction.Right
-        Direction.Right -> Direction.Left
-        Direction.Top -> Direction.Bottom
-        Direction.Bottom -> Direction.Top
         else -> error("Unhandled directional change")
     }
 }
@@ -58,121 +65,115 @@ fun main() {
 
     val puzzleInput = "day22/input.txt".loadResource()
 
-    fun String.parseData(): Pair<Bitmap, String> = this.split("\n\n").zipWithNext { m, i ->
-        m.split('\n').map { it.toCharArray() }.toTypedArray() to i
-    }.first()
-
-    fun Bitmap.tileAt(p: Point): Char? = if (p.x < 0 || p.y < 0 || p.y >= size || p.x >= this[p.y].size) null else this[p.y][p.x]
-
-    fun Bitmap.printMapAndPosition(point: Point, side: Int = 0) {
-        val mapCopy = Array(this.size) { this[it].copyOf() }
-        mapCopy[point.y][point.x] = '*'
-        println("Side: $side")
-        mapCopy.forEach { println(it) }
-    }
-
-    fun <T> List<T>.countUntil(predicate: (T) -> Boolean): Int {
-        var count = 0
-        while (!predicate(this[count])) count++
-        return count
-    }
-
-    fun Bitmap.sliceY(x: Int) = map { if (x >= it.size) ' ' else it[x] }
-
-    fun List<Char>.trim(): List<Char> = this.filter { it != ' ' }
-
-    fun CharArray.countUntil(predicate: (Char) -> Boolean): Int {
-        var count = 0
-        while (!predicate(this[count])) count++
-        return count
-    }
-
-    fun Bitmap.walk(instructions: List<Instruction>): Int {
-        var position = Point(this[0].indexOf('.'), 0)
-        var heading = Direction.Right
-
-        instructions.forEach { instruction ->
-            when (instruction) {
-                is Instruction.Move -> {
-                    var count = 0
-                    while (count < instruction.amount) {
-                        println("Pos: $position Heading: $heading")
-                        var newPosition = position.move(heading)
-                        while (true) {
-                            when (tileAt(newPosition)) {
-                                '.' -> {
-                                    position = newPosition
-                                    break
-                                }
-
-                                '#' -> {
-                                    println("Bump wall")
-                                    break
-                                }
-
-                                null, ' ' -> {
-                                    println("Wrapped $newPosition")
-                                    when (heading) {
-                                        Direction.Bottom, Direction.Top -> {
-                                            // Wrap Y
-                                            val yslice = sliceY(newPosition.x)
-                                            val leadingYSpaces = yslice.countUntil { it != ' ' }
-                                            val newY = newPosition.y.mod(yslice.trim().size) + leadingYSpaces
-                                            newPosition = Point(newPosition.x, newY)
-                                        }
-
-                                        Direction.Left, Direction.Right -> {
-                                            // Wrap X
-                                            val leadingXSpaces = this[newPosition.y].countUntil { it != ' ' }
-                                            val newX = newPosition.x.mod(this[newPosition.y].size - leadingXSpaces) + leadingXSpaces
-                                            newPosition = Point(newX, newPosition.y)
-                                        }
-
-                                        else -> error("Where am I?")
-                                    }
-                                    // Try checking new position again
-                                }
-                            }
-                        }
-                        count++
-                    }
-                }
-
-                is Instruction.Turn -> {
-                    heading += instruction.direction
-                    println("Turn ${instruction.direction.name} now facing ${heading.name}")
-                }
-            }
-        }
-
-        return (position.y + 1) * 1000 + 4 * (position.x + 1) + when (heading) {
-            Direction.Right -> 0
-            Direction.Bottom -> 1
-            Direction.Left -> 2
-            Direction.Top -> 3
-            else -> error("Where am I?")
-        }
-    }
-
-    fun String.splitInstructions(): List<Instruction> = sequence<Instruction> {
-        val st = StringTokenizer(this@splitInstructions, "LR", true)
+    fun String.parseInstructions(): List<Instruction> = sequence<Instruction> {
+        val st = StringTokenizer(this@parseInstructions, "LR", true)
         while (st.hasMoreTokens()) {
-            val token = st.nextToken().trim()
-            if (token == "R" || token == "L") {
-                val direction = when (token) {
-                    "R" -> Direction.Right
-                    "L" -> Direction.Left
-                    else -> error("Unknown direction")
+            yield(
+                when (val token = st.nextToken().trim()) {
+                    "R" -> Instruction.Right
+                    "L" -> Instruction.Left
+                    else -> Instruction.Forward(token.toInt())
                 }
-                yield(Instruction.Turn(direction))
-            } else {
-                yield(Instruction.Move(token.toInt()))
-            }
+            )
         }
     }.toList()
 
+    fun CharArray.wallPoints(y: Int): Set<Point> = toList().mapIndexedNotNull { x, cell -> if (cell == '#') Point(x, y) else null }.toSet()
+
+    fun Bitmap.at(p: Point): Char? = if (p.x < 0 || p.y < 0 || p.y >= this.size || p.x >= this[p.y].size) null else this[p.y][p.x]
+
+    fun Bitmap.walkPerimeter(initialPosition: Point): List<Location> = buildList {
+        var position = initialPosition
+        var heading = Direction.Right
+        do {
+            add(Location(position, heading))
+            val forward = position.move(heading)
+            val cell = at(forward)
+            if (cell == null || cell == ' ') {
+                heading += Direction.Right
+            } else {
+                val left = forward.move(heading + Direction.Left)
+                val leftCell = at(left)
+                if (leftCell == null || leftCell == ' ') {
+                    position = forward
+                } else {
+                    position = left
+                    heading += Direction.Left
+                }
+            }
+        } while (position != initialPosition || heading != Direction.Right)
+    }
+
+    fun Board.wrap2D(): Board = copy(adjacent = perimeter.associate { location ->
+        val p = location.position
+        val d = location.direction + Direction.Left
+        val wrapPosition = when (d) {
+            Direction.Right -> Point(x = map[p.y].indexOfFirst { it != ' ' }, y = p.y)
+            Direction.Bottom -> Point(x = p.x, y = map.indexOfFirst { p.x in it.indices && it[p.x] != ' ' })
+            Direction.Left -> Point(x = map[p.y].indexOfLast { it != ' ' }, y = p.y)
+            Direction.Top -> Point(x = p.x, y = map.indexOfLast { p.x in it.indices && it[p.x] != ' ' })
+            else -> error("Unknown direction")
+        }
+        location.copy(direction = d) to Location(wrapPosition, d)
+    })
+
+    fun String.parseData(): Pair<Board, List<Instruction>> = this.split("\n\n").zipWithNext { m, i ->
+        val map: Bitmap = m.split('\n').map { it.toCharArray() }.toTypedArray()
+        val initialPosition = Point(map[0].indexOf('.'), 0)
+        val perimeter = map.walkPerimeter(initialPosition)
+        val walls = map.flatMapIndexed { y, line -> line.wallPoints(y) }.toSet()
+        Board(map, initialPosition, perimeter, walls) to i.parseInstructions()
+    }.first()
+
+    fun Direction.toInt(): Int = when (this) {
+        Direction.Right -> 0
+        Direction.Bottom -> 1
+        Direction.Left -> 2
+        Direction.Top -> 3
+        else -> error("Where am I?")
+    }
+
+    fun Board.show(location: Location) {
+        println("==========")
+        map.forEachIndexed { index, row ->
+            println(
+                if (index == location.position.y) {
+                    row.copyOf().also {
+                        it[location.position.x] = when (location.direction) {
+                            Direction.Top -> '^'
+                            Direction.Left -> '<'
+                            Direction.Right -> '>'
+                            Direction.Bottom -> 'v'
+                            else -> '*'
+                        }
+                    }
+                } else {
+                    row
+                }.joinToString("")
+            )
+        }
+    }
+
+    fun List<Instruction>.walk(board: Board): Int {
+        return fold(Location(board.initialPosition, Direction.Right)) outer@{ location, instruction ->
+            when (instruction) {
+                is Instruction.Forward -> {
+                    (1..instruction.count).fold(location) { location, _ ->
+                        val next = board.adjacent[location] ?: location.copy(position = location.position.move(location.direction))
+                        if (board.map.at(next.position) != '.') location else next
+                    }
+                }
+
+                Instruction.Left -> location.copy(direction = location.direction + Direction.Left)
+                Instruction.Right -> location.copy(direction = location.direction + Direction.Right)
+            }
+        }.let {
+            1000 * (it.position.y + 1) + 4 * (it.position.x + 1) + it.direction.toInt()
+        }
+    }
+
     // Part 1
-    fun Pair<Bitmap, String>.part1(): Int = first.walk(second.splitInstructions())
+    fun Pair<Board, List<Instruction>>.part1(): Int = second.walk(first.wrap2D())
 
     with(sampleInput.parseData()) {
         Assert.equals(part1(), 6032)
@@ -180,134 +181,55 @@ fun main() {
     }
 
     with(puzzleInput.parseData()) {
-        println("Part 1 result: ${part1()}")
+        val result = part1()
+        Assert.equals(result, 73346)
+        println("Part 1 result: $result")
     }
 
-    // Part 2
-    fun Bitmap.toCube(): Array<Bitmap> {
-        val cubeSize = size / 3
-        var y = 0
-        val cube = Array<Bitmap>(6) { Bitmap(cubeSize) { CharArray(cubeSize) } }
-        // Top
-        for (y in 0 until cubeSize) {
-            cube[0][y] = this[y].toList().trim().toCharArray()
-            cube[1][y] = this[y + cubeSize].toList().trim().take(cubeSize).toCharArray()
-            cube[2][y] = this[y + cubeSize].toList().trim().drop(cubeSize).take(cubeSize).toCharArray()
-            cube[3][y] = this[y + cubeSize].toList().trim().drop(cubeSize + cubeSize).take(cubeSize).toCharArray()
-            cube[4][y] = this[y + cubeSize + cubeSize].toList().trim().take(cubeSize).toCharArray()
-            cube[5][y] = this[y + cubeSize + cubeSize].toList().trim().drop(cubeSize).take(cubeSize).toCharArray()
+    fun Board.wrapCube(): Board = copy(adjacent = buildMap {
+        var sideLength = Int.MAX_VALUE
+        var previousDirection = Direction.Right
+        var directionCount = 0
+        for ((_, direction) in perimeter) {
+            if (direction == previousDirection) {
+                directionCount++
+            } else {
+                sideLength = minOf(sideLength, directionCount)
+                previousDirection = direction
+                directionCount = 1
+            }
         }
-        return cube
-    }
-
-    fun cube(direction: Direction, prevSide: Int): Pair<Int, Direction> {
-        val lookup = mapOf(
-            Direction.Left to mapOf(
-                1 to (3 to Direction.Right),
-                2 to (6 to Direction.Left),
-                3 to (2 to Direction.Top),
-                4 to (3 to Direction.Top),
-                5 to (3 to Direction.Left),
-                6 to (5 to Direction.Top),
-            ),
-            Direction.Right to mapOf(
-                1 to (6 to Direction.Bottom),
-                2 to (3 to Direction.Top),
-                3 to (4 to Direction.Top),
-                4 to (6 to Direction.Left),
-                5 to (6 to Direction.Top),
-                6 to (1 to Direction.Bottom),
-            ),
-            Direction.Top to mapOf(
-                1 to (2 to Direction.Bottom),
-                2 to (5 to Direction.Top),
-                3 to (1 to Direction.Left),
-                4 to (1 to Direction.Top),
-                5 to (4 to Direction.Top),
-                6 to (4 to Direction.Right),
-            ),
-            Direction.Bottom to mapOf(
-                1 to (4 to Direction.Top),
-                2 to (5 to Direction.Bottom),
-                3 to (5 to Direction.Right),
-                4 to (5 to Direction.Top),
-                5 to (2 to Direction.Bottom),
-                6 to (2 to Direction.Right),
-            )
-        )
-        return lookup[direction]!![prevSide + 1]?.let { it.first - 1 to it.second } ?: error("Wut")
-    }
-
-    fun Point.wrapCube(side: Int, cubeSize: Int): Pair<Point, Pair<Int, Direction>> {
-        return when {
-            x < 0 -> Point(cubeSize - 1, y) to cube(Direction.Left, side)
-            x >= cubeSize -> Point(0, y) to cube(Direction.Right, side)
-            y < 0 -> Point(x, cubeSize - 1) to cube(Direction.Top, side)
-            y >= cubeSize -> Point(x, 0) to cube(Direction.Bottom, side)
-            else -> this to (side to Direction.Top)
+        sideLength = minOf(sideLength, directionCount)
+        val unpairedEdges = MutableList(perimeter.size / sideLength) { i ->
+            perimeter[i * sideLength].direction to perimeter.subList(i * sideLength, (i + 1) * sideLength)
         }
-    }
-
-    fun Array<Bitmap>.walk(instructions: List<Instruction>): Int {
-        var side = 0
-        var position = Point(0, 0)
-        var heading = Direction.Right
-        val cubeSize = this[0].size
-
-        instructions.forEach { instruction ->
-            when (instruction) {
-                is Instruction.Move -> {
-                    var count = 0
-                    while (count < instruction.amount) {
-                        println("Pos: $position Heading: $heading")
-                        this[side].printMapAndPosition(position, side)
-                        val (newPosition, newSideAndDirection) = position.move(heading).wrapCube(side, cubeSize)
-                        val newSide = newSideAndDirection.first
-                        val newDirection = newSideAndDirection.second
-                        heading = when (newDirection) {
-                            Direction.Top -> heading // No change 0 deg
-                            Direction.Left -> heading + Direction.Right
-                            Direction.Right -> heading + Direction.Left
-                            Direction.Bottom -> heading + Direction.Right + Direction.Right
-                            else -> error("Unexpected change in direction")
-                        }
-                        while (true) {
-                            when (val tile = this[newSide].tileAt(newPosition)) {
-                                '.' -> {
-                                    position = newPosition
-                                    side = newSide
-                                    break
-                                }
-
-                                '#' -> {
-                                    println("Bump wall")
-                                    break
-                                }
-
-                                else -> error("Unknown tile [$tile] Side: $newSide Position: $newPosition")
-                            }
-                        }
-                        count++
+        while (unpairedEdges.isNotEmpty()) {
+            var i = 0
+            while (i < unpairedEdges.lastIndex) {
+                val a = unpairedEdges[i]
+                val b = unpairedEdges[i + 1]
+                if (a.first + Direction.Left == b.first) {
+                    unpairedEdges.subList(i, i + 2).clear()
+                    for (j in i..unpairedEdges.lastIndex) {
+                        val edge = unpairedEdges[j]
+                        unpairedEdges[j] = edge.copy(first = edge.first + Direction.Left)
                     }
-                }
-
-                is Instruction.Turn -> {
-                    heading += instruction.direction
-                    println("Turn ${instruction.direction.name} now facing ${heading.name}")
+                    val edge1 = a.second
+                    val edge2 = b.second
+                    for (j in 0 until sideLength) {
+                        val (position1, direction1) = edge1[j]
+                        val (position2, direction2) = edge2[sideLength - j - 1]
+                        this[Location(position1, direction1 + Direction.Left)] = Location(position2, direction2 + Direction.Right)
+                        this[Location(position2, direction2 + Direction.Left)] = Location(position1, direction1 + Direction.Right)
+                    }
+                } else {
+                    i++
                 }
             }
         }
+    })
 
-        return (position.y + 1) * 1000 + 4 * (position.x + 1) + when (heading) {
-            Direction.Right -> 0
-            Direction.Bottom -> 1
-            Direction.Left -> 2
-            Direction.Top -> 3
-            else -> error("Where am I?")
-        }
-    }
-
-    fun Pair<Bitmap, String>.part2(): Int = first.toCube().walk(second.splitInstructions())
+    fun Pair<Board, List<Instruction>>.part2(): Int = second.walk(first.wrapCube())
 
     with(sampleInput.parseData()) {
         Assert.equals(part2(), 5031)
